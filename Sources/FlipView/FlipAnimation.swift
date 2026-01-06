5cc63ab2b7f7e8f74c3c7ef5cc640a7ccc8b5c53
Daniel Saidi <daniel.saidi@gmail.com>
2026-01-06 09:04:26 +0100

    Update copyright info

--- Sources/FlipView/FlipAnimation.swift
+++ Sources/FlipView/FlipAnimation.swift
@@ -3,7 +3,7 @@
 //  FlipKit
 //
 //  Created by Daniel Saidi on 2025-07-14.
-//  Copyright © 2025 Daniel Saidi. All rights reserved.
+//  Copyright © 2025-2026 Daniel Saidi. All rights reserved.
 //
 
 import SwiftUI
